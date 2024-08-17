2e67150f475cf73756e12674636af440b0facb2b
Vikas <vikasaggarwal3213@gmail.com>
2024-08-17 17:19:15 +0530

    test

--- tests/orangeHRM/admin/AddEmployee.test.ts
+++ tests/orangeHRM/admin/AddEmployee.test.ts
@@ -1,7 +1,7 @@
 import { expect } from '@playwright/test';
 import test from '@lib/BaseTest';
 
-test('Verify admin can add new employees', { tag: '@smoke' }, async ({page, loginPage, pimPage }) => {
+test.only('Verify admin can add new employees', { tag: '@smoke' }, async ({page, loginPage, pimPage }) => {
   //Login
   await loginPage.login();
   //Add new employee
